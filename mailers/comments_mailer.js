d66f9cb66c975a962e1f8d5a8cf4f1e1aca20900
Amit Vishwakarma <amit200698@gmail.com>
2023-05-09 19:40:09 +0530

    here we have write social media code till nodemailer

---
+++ mailers/comments_mailer.js
@@ -0,0 +1,19 @@
+const nodemailer = require('../config/nodemailers');
+
+// this is another way to exporting method
+exports.newComment = (comment) =>{
+    let htmlString = nodemailer.renderTemplate({comment: comment}, 'comments/new_comment.ejs')
+
+    nodemailer.transporter.sendMail({
+        from: 'av7942686@gmail.com',
+        to: comment.user.email,
+        subject: 'new Comment published!',
+        html: htmlString
+    },(err, info)=>{
+        if(err){
+            console.log('error in sending mail', err);
+            return;
+        }
+        console.log('message is sent', info);
+    });
+}
