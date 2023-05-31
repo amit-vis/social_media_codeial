c0134af999df197e8a90d1b344482279f44035eb
Amit Vishwakarma <amit200698@gmail.com>
2023-05-31 10:39:34 +0530

    created chat engine

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -16,5 +16,18 @@ module.exports.chatSockets = function(socketServer){
         socket.on('disconnect', function(){
             console.log('socket disconnected!');
         });
+
+        socket.on('join_room', function(data){
+            console.log('joining request receive', data);
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit('user_joined', data);
+        });
+
+        socket.on('send_message', function(data){
+            io.in(data.chatroom).emit('receive_message', data);
+        })
+
     });
 }
